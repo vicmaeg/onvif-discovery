e03847056e560fa71ecfae17c3dee87b55f4b604
Victor Martos <vmartos@outlook.com>
2019-09-15 00:40:55 +0200

    Output again the XAddress in the CLI program

--- OnvifSharp.CLI/Program.cs
+++ OnvifSharp.CLI/Program.cs
@@ -8,11 +8,16 @@ namespace OnvifSharp.CLI
 	{
 		static async Task Main ()
 		{
-			Console.WriteLine ("Starting Discover ONVIF cameras!");
+			Console.WriteLine ("Starting Discover ONVIF cameras!\n");
 			var discovery = new WSDiscovery ();
 			var devices = await discovery.Discover (1);
 			foreach(var device in devices) {
-				Console.WriteLine ($"Device model {device.Model} has address {device.Address}");
+				Console.WriteLine ($"Device model {device.Model} from manufacturer {device.Mfr} has address {device.Address}");
+				Console.Write ($"Urls to device: ");
+				foreach (var address in device.XAdresses) {
+					Console.Write ($"{address}, ");
+				}
+				Console.WriteLine ("\n");
 			}
 			Console.WriteLine ("ONVIF Discovery finished");
 		}
