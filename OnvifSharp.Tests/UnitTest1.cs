5107a2015f04004d0286d0e8b7b6081de4c62c79
Victor Martos <vmartos@outlook.com>
2019-09-19 23:18:18 +0200

    Change NUnit to XUnit test lib

--- OnvifSharp.Tests/UnitTest1.cs
+++ OnvifSharp.Tests/UnitTest1.cs
@@ -1,18 +1,14 @@
-using NUnit.Framework;
+using System;
+using Xunit;
 
-namespace Tests
+namespace OnvifSharp.Tests
 {
-    public class Tests
+    public class UnitTest1
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
-        [Test]
+        [Fact]
         public void Test1()
         {
-            Assert.Pass();
+			Assert.True (false);
         }
     }
 }
