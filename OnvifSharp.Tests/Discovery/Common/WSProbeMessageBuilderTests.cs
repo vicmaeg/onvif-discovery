c5bda3654deb3327121491dd3cd02d3d57c4643f
Victor Martos <vmartos@outlook.com>
2019-11-18 22:43:13 +0100

    Add WSProbeMessageBuilder tests

---
+++ OnvifSharp.Tests/Discovery/Common/WSProbeMessageBuilderTests.cs
@@ -0,0 +1,30 @@
+using System;
+using System.IO;
+using System.Text;
+using System.Xml;
+using FluentAssertions;
+using OnvifSharp.Discovery.Common;
+using Xunit;
+
+namespace OnvifSharp.Tests.Discovery.Common
+{
+	public class WSProbeMessageBuilderTests
+	{
+		[Fact]
+		public void NewProbeMessage_MessageIdValid_AddsMessageId ()
+		{
+			Guid messageId = Guid.NewGuid ();
+			var messageBytes = WSProbeMessageBuilder.NewProbeMessage (messageId);
+			var message = Encoding.UTF8.GetString (messageBytes);
+			message.Should ().Contain (messageId.ToString ());
+		}
+
+		[Fact]
+		public void NewProbeMessage_MessageIdEmpty_ThrowsInvalidArgumentException ()
+		{
+			Action act = () => WSProbeMessageBuilder.NewProbeMessage (Guid.Empty);
+
+			act.Should ().Throw<ArgumentException> ().WithMessage ("messageId could not be Empty");
+		}
+	}
+}
