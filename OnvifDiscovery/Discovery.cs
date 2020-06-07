1a46d5a7f0015d850aa17aa126474ec6b353f18b
Victor Martos <vmartos@outlook.com>
2020-06-07 21:52:09 +0200

    Update documentation

--- OnvifDiscovery/Discovery.cs
+++ OnvifDiscovery/Discovery.cs
@@ -42,12 +42,13 @@ namespace OnvifDiscovery
 		}
 
 		/// <summary>
-		/// Discover new onvif devices on the network
+		/// Discover new onvif devices on the network passing a callback
+		/// to retrieve devices as they reply
 		/// </summary>
 		/// <param name="timeout">A timeout in seconds to wait for onvif devices</param>
 		/// <param name="onDeviceDiscovered">A method that is called each time a new device replies.</param>
 		/// <param name="cancellationToken">A cancellation token</param>
-		/// <returns>a list of <see cref="DiscoveryDevice"/></returns>
+		/// <returns>The Task to be awaited</returns>
 		public async Task Discover (int timeout, Action<DiscoveryDevice> onDeviceDiscovered,
 			CancellationToken cancellationToken = default)
 		{
@@ -62,12 +63,13 @@ namespace OnvifDiscovery
 		}
 
 		/// <summary>
-		/// Discover new onvif devices on the network.
-		/// Use the <see cref="Discover(int, Action{DiscoveryDevice}, CancellationToken)"/> overload (with an action as a parameter) if you want to retrieve devices as they reply.
+		/// Discover new onvif devices on the network
 		/// </summary>
 		/// <param name="timeout">A timeout in seconds to wait for onvif devices</param>
 		/// <param name="cancellationToken">A cancellation token</param>
 		/// <returns>a list of <see cref="DiscoveryDevice"/></returns>
+		/// <remarks>Use the <see cref="Discover(int, Action{DiscoveryDevice}, CancellationToken)"/> 
+		///  overload (with an action as a parameter) if you want to retrieve devices as they reply.</remarks>
 		public async Task<IEnumerable<DiscoveryDevice>> Discover (int timeout, CancellationToken cancellationToken = default)
 		{
 			var devices = new List<DiscoveryDevice> ();
--- OnvifDiscovery/Interfaces/IDiscovery.cs
+++ OnvifDiscovery/Interfaces/IDiscovery.cs
@@ -1,4 +1,5 @@
 ﻿using OnvifDiscovery.Models;
+using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
@@ -11,11 +12,24 @@ namespace OnvifDiscovery.Interfaces
 	public interface IDiscovery
 	{
 		/// <summary>
+		/// Discover new onvif devices on the network passing a callback
+		/// to retrieve devices as they reply
+		/// </summary>
+		/// <param name="timeout">A timeout in seconds to wait for onvif devices</param>
+		/// <param name="onDeviceDiscovered">A method that is called each time a new device replies.</param>
+		/// <param name="cancellationToken">A cancellation token</param>
+		/// <returns>The Task to be awaited</returns>
+		Task Discover (int timeout, Action<DiscoveryDevice> onDeviceDiscovered,
+			CancellationToken cancellationToken = default);
+
+		/// <summary>
 		/// Discover new onvif devices on the network
 		/// </summary>
 		/// <param name="timeout">A timeout in seconds to wait for onvif devices</param>
 		/// <param name="cancellationToken">A cancellation token</param>
 		/// <returns>a list of <see cref="DiscoveryDevice"/></returns>
+		/// <remarks>Use the <see cref="Discover(int, Action{DiscoveryDevice}, CancellationToken)"/> 
+		///  overload (with an action as a parameter) if you want to retrieve devices as they reply.</remarks>
 		Task<IEnumerable<DiscoveryDevice>> Discover (int timeout,
 			CancellationToken cancellationToken = default);
 	}
