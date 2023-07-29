d46aaed3c7036e27a40e34ab3ed540e8a6052ea3
vicmaeg <vmartos@outlook.com>
2023-07-29 23:37:09 +0200

    chore: modify Program.cs to use Top-level statements

--- OnvifDiscovery.CLI/Program.cs
+++ OnvifDiscovery.CLI/Program.cs
@@ -1,39 +1,32 @@
-﻿using OnvifDiscovery.Models;
+﻿using OnvifDiscovery;
+using OnvifDiscovery.Models;
 
-namespace OnvifDiscovery.CLI;
+Console.WriteLine("Starting Discover ONVIF cameras for 10 seconds, press Ctrl+C to abort\n");
 
-internal class Program
+var cts = new CancellationTokenSource();
+Console.CancelKeyPress += (s, e) =>
 {
-    private static async Task Main()
+    e.Cancel = true;
+    cts.Cancel();
+};
+var discovery = new Discovery();
+await discovery.Discover(10, onNewDevice, cts.Token);
+Console.WriteLine("ONVIF Discovery finished");
+
+void onNewDevice(DiscoveryDevice device)
+{
+    // Multiple events could be received at the same time.
+    // The lock is here to avoid messing the console.
+    lock (Console.Out)
     {
-        Console.WriteLine("Starting Discover ONVIF cameras for 10 seconds, press Ctrl+C to abort\n");
-
-        var cts = new CancellationTokenSource();
-        Console.CancelKeyPress += (s, e) =>
+        Console.WriteLine(
+            $"Device model {device.Model} from manufacturer {device.Mfr} has address {device.Address}");
+        Console.Write("Urls to device: ");
+        foreach (var address in device.XAdresses)
         {
-            e.Cancel = true;
-            cts.Cancel();
-        };
-        var discovery = new Discovery();
-        await discovery.Discover(10, OnNewDevice, cts.Token);
-        Console.WriteLine("ONVIF Discovery finished");
-    }
-
-    private static void OnNewDevice(DiscoveryDevice device)
-    {
-        // Multiple events could be received at the same time.
-        // The lock is here to avoid messing the console.
-        lock (Console.Out)
-        {
-            Console.WriteLine(
-                $"Device model {device.Model} from manufacturer {device.Mfr} has address {device.Address}");
-            Console.Write("Urls to device: ");
-            foreach (var address in device.XAdresses)
-            {
-                Console.Write($"{address}, ");
-            }
-
-            Console.WriteLine("\n");
+            Console.Write($"{address}, ");
         }
+
+        Console.WriteLine("\n");
     }
 }
